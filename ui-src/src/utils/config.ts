d22ea19e09d24dfb8ef637c44cd496cab718a78b
hsingla005 <hsingla@twilio.com>
2024-02-19 23:47:33 +0530

    FLEXY-4997 updated assets

--- ui-src/src/utils/config.ts
+++ ui-src/src/utils/config.ts
@@ -1,6 +1,6 @@
 import { ITask } from '@twilio/flex-ui';
 import * as Flex from '@twilio/flex-ui';
-import { TaskQualificationConfig } from '../types/ServiceConfiguration';
+import AgentAutomationConfig, { TaskQualificationConfig } from '../types/ServiceConfiguration';
 
 interface TaskAttributesQualificationConfig {
   key: string;
@@ -10,19 +10,21 @@ interface TaskAttributesQualificationConfig {
 type FlexUIAttributes = Flex.ServiceConfiguration['ui_attributes'];
 
 interface UIAttributes extends FlexUIAttributes {
-  agent_automation: {
-    configuration: any;
+  custom_data: {
+    features: any;
   };
 }
 
-const configuration = Flex?.Manager?.getInstance()?.configuration as UIAttributes;
+const { custom_data } = Flex.Manager.getInstance().configuration as UIAttributes;
+
+const { configuration = [] } = (custom_data?.features?.agent_automation as AgentAutomationConfig) || {};
 
 export const getMatchingTaskConfiguration = (task: ITask): TaskQualificationConfig | null => {
   const { taskChannelUniqueName: channel } = task;
   const attributes = task.attributes as any;
   let first_matched_config = null as TaskQualificationConfig | null;
 
-  configuration.agent_automation.configuration.forEach((config: any) => {
+  configuration.forEach((config: any) => {
     let matched_config = true;
     if (config.channel === channel) {
       config.required_attributes?.forEach((required_attribute: TaskAttributesQualificationConfig) => {
